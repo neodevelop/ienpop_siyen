acb1602d1b5232a2830e6705068196fbf20cf709
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2008-01-05 10:48:57 +0000

    Un service para obtener mas datos y redireccion del view
    git-svn-id: http://192.168.1.94/svn@71 d43fff5d-296d-41c8-bd8c-7388abecf751

--- IenpopWeb/src/main/java/edu/ienpop/mvc/controller/CertificadosController.java
+++ IenpopWeb/src/main/java/edu/ienpop/mvc/controller/CertificadosController.java
@@ -6,14 +6,20 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+import edu.ienpop.model.CatalogoCurso;
+import edu.ienpop.model.CatalogoPuerto;
+import edu.ienpop.model.Curso;
+import edu.ienpop.services.CursoService;
 import edu.ienpop.services.PersistenceService;
 
 public class CertificadosController extends AbstractController {
 
+	CursoService cursoService;
 	PersistenceService persistenceService;
 	public PersistenceService getPersistenceService() {
 		return persistenceService;
@@ -21,14 +27,28 @@ public class CertificadosController extends AbstractController {
 	public void setPersistenceService(PersistenceService persistenceService) {
 		this.persistenceService = persistenceService;
 	}
+	public CursoService getCursoService() {
+		return cursoService;
+	}
+	public void setCursoService(CursoService cursoService) {
+		this.cursoService = cursoService;
+	}
 	@SuppressWarnings("unchecked")
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		String noCurso = ServletRequestUtils.getStringParameter(request, "noCurso");
+		long idCurso = ServletRequestUtils.getLongParameter(request, "idCurso");
+		Curso curso =  getCursoService().generateCertificadosXCurso(idCurso);
+		System.out.println(ToStringBuilder.reflectionToString(curso));
+		CatalogoPuerto puerto = (CatalogoPuerto)getPersistenceService().findById(CatalogoPuerto.class, curso.getIdPuerto());
+		CatalogoCurso tipoCurso = (CatalogoCurso)getPersistenceService().findById(CatalogoCurso.class, curso.getIdTipoCurso());
 		Map model = new HashMap();
-		model.put("noCurso", noCurso);
-		return new ModelAndView("certificados",model);
+		model.put("idCurso", idCurso);
+		model.put("curso",curso);
+		model.put("puerto",puerto);
+		model.put("tipoCurso",tipoCurso);
+		model.put("alumnos", curso.getAlumnos());
+		return new ModelAndView("constancias",model);
 	}
 
 }
