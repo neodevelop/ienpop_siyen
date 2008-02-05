0d7cae2a7ee8905f2cc78447ebddf3ba993ca857
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2008-02-05 06:58:16 +0000

    Logger agregado y se hace una llamada con un catch de businessException para tomarla y sacarla al view
    git-svn-id: http://192.168.1.94/svn@126 d43fff5d-296d-41c8-bd8c-7388abecf751

--- IenpopWeb/src/main/java/edu/ienpop/mvc/controller/SistemaController.java
+++ IenpopWeb/src/main/java/edu/ienpop/mvc/controller/SistemaController.java
@@ -6,12 +6,14 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import edu.ienpop.integration.jms.IenpopProducer;
 import edu.ienpop.model.Usuario;
+import edu.ienpop.services.BusinessException;
 import edu.ienpop.services.CatalogoService;
 import edu.ienpop.services.PersistenceService;
 
@@ -20,6 +22,7 @@ public class SistemaController extends AbstractController {
 	PersistenceService persistenceService;
 	IenpopProducer ienpopProducer;
 	CatalogoService catalogoService;
+	Logger log = Logger.getLogger(this.getClass());
 
 	public CatalogoService getCatalogoService() {
 		return catalogoService;
@@ -45,7 +48,7 @@ public class SistemaController extends AbstractController {
 		String idUsuario = ServletRequestUtils.getStringParameter(request,
 				"usuario", "none");
 		if (idUsuario.equals("none")) {
-			model.put("error", "La sesi√≥n ha expirado o el ingreso fue incorrecto...");
+			model.put("error", "La sesión ha expirado o el ingreso fue incorrecto...");
 			return new ModelAndView("main",model);
 		} else {
 			Usuario usuario = (Usuario) getPersistenceService().findById(
@@ -54,7 +57,13 @@ public class SistemaController extends AbstractController {
 			model
 					.put("catalogoCursos", getCatalogoService()
 							.getCatalogoCurso());
-			ienpopProducer.notificarAcceso(usuario);
+			try{
+				ienpopProducer.notificarAcceso(usuario);
+			}catch(BusinessException e){
+				model.put("error", "Error de comunicaciones con el servidor...");
+				log.debug("Error de comunicación:"+e.getMessage());
+				return new ModelAndView("main",model);
+			}
 			return new ModelAndView("sistema", model);
 		}
 	}
