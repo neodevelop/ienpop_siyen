ca8e9ccb05fd944a08827024d57718f821abc82d
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2008-06-06 19:25:48 +0000

    un log.debug para tomar los errores en el log...
    git-svn-id: http://192.168.1.94/svn@236 d43fff5d-296d-41c8-bd8c-7388abecf751

--- IenpopServices/src/main/java/edu/ienpop/advices/ServiceThrowsAdvice.java
+++ IenpopServices/src/main/java/edu/ienpop/advices/ServiceThrowsAdvice.java
@@ -1,5 +1,6 @@
 package edu.ienpop.advices;
 
+import org.apache.log4j.Logger;
 import org.hibernate.LazyInitializationException;
 import org.hibernate.exception.GenericJDBCException;
 import org.springframework.dao.DataAccessException;
@@ -9,19 +10,25 @@ import edu.ienpop.services.BusinessException;
 
 public class ServiceThrowsAdvice{
 
+	Logger log = Logger.getLogger(this.getClass());
+	
 	public void doRecoveryActionDataAccess(DataAccessException dataAccessEx) throws BusinessException{
+		log.debug(dataAccessEx.getMessage());
 		throw new BusinessException("Error en Persistencia: ",dataAccessEx);
 	}
 	
 	public void doRecoveryActionGenericJDBC(GenericJDBCException jdbcException) throws BusinessException{
+		log.debug(jdbcException.getMessage());
 		throw new BusinessException("Error en Persistencia: ",jdbcException);
 	}
 	
 	public void doRecoveryActionNetConnection(UncategorizedJmsException jmsException) throws BusinessException{
+		log.debug(jmsException.getMessage());
 		throw new BusinessException("Error de Comunicaci—n: ",jmsException);
 	}
 	
 	public void doRecoveryActionLazy(LazyInitializationException lazyException) throws BusinessException{
+		log.debug(lazyException.getMessage());
 		throw new BusinessException("Error en Persistencia: ",lazyException);
 	}
 	
