e7624de5cac4bd63434ba8c64f8e32d6770b9ddb
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2008-06-06 19:28:40 +0000

    Implementación de 4 tipos de busqueda, por curso y por alumnos, por curso con libreta y por alumnos con libreta
    git-svn-id: http://192.168.1.94/svn@239 d43fff5d-296d-41c8-bd8c-7388abecf751

--- IenpopWeb/src/main/java/edu/ienpop/mvc/controller/InformePeriodicoController.java
+++ IenpopWeb/src/main/java/edu/ienpop/mvc/controller/InformePeriodicoController.java
@@ -14,14 +14,25 @@ import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+import edu.ienpop.model.CatalogoPuerto;
 import edu.ienpop.model.CursoCriteria;
 import edu.ienpop.services.CursoService;
+import edu.ienpop.services.PersistenceService;
 
 public class InformePeriodicoController extends AbstractController {
 
 	Logger log = Logger.getLogger(this.getClass());
 	private CursoService cursoService;
+	private PersistenceService persistenceService;
 	
+	public PersistenceService getPersistenceService() {
+		return persistenceService;
+	}
+
+	public void setPersistenceService(PersistenceService persistenceService) {
+		this.persistenceService = persistenceService;
+	}
+
 	public CursoService getCursoService() {
 		return cursoService;
 	}
@@ -34,14 +45,22 @@ public class InformePeriodicoController extends AbstractController {
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
+		log.debug("En InformePeriodicoController...");
 		CursoCriteria cursoCriteria = new CursoCriteria();
+		Map model = new HashMap();
+		//Todos estos cursos deben de estar concluidos
+		cursoCriteria.setIdStatusCurso(CursoCriteria.CONCLUIDO);
 		int tipo = ServletRequestUtils.getIntParameter(request, "tipo", 1);
 		String view = "";
 		String desde = ServletRequestUtils.getStringParameter(request, "desde", "none");
 		String hasta = ServletRequestUtils.getStringParameter(request, "hasta", "none");
 		cursoCriteria = setterFechas(desde, hasta, cursoCriteria);
 		List cursos = null;
-		String idPuerto,libreta;
+		String idPuerto=null;
+		String libreta=null;
+		boolean verLibretas = false;
+		CatalogoPuerto puerto = null;
+		String[] libretas;
 		switch(tipo){
 		case 1:
 			cursos = getCursoService().getInformePeriodico(cursoCriteria.getFechaDesde(), cursoCriteria.getFechaHasta());
@@ -53,29 +72,49 @@ public class InformePeriodicoController extends AbstractController {
 			break;
 		case 3:
 			idPuerto = ServletRequestUtils.getStringParameter(request, "idPuerto", "none");
+			//Obtenemos las libretas del request
 			libreta = ServletRequestUtils.getStringParameter(request, "libreta");
+			//log.debug("El valor de 'libreta' es: "+libreta);
+			//Separamos en un arreglo
+			libretas = libreta.split(",");
+			log.debug(ToStringBuilder.reflectionToString(libretas));
 			cursoCriteria.setIdPuerto(new String[] {idPuerto});
-			cursoCriteria.setIdStatusCurso(CursoCriteria.CONCLUIDO);
+			log.debug("Puerto: "+idPuerto);
+			puerto = (CatalogoPuerto)getPersistenceService().findById(CatalogoPuerto.class, idPuerto);
+			//Se lo ponemos a criteria
+			cursoCriteria.setLibretas(libretas);
+			//Lo mandamos al servicio
 			cursos = getCursoService().getCursosXStatus(cursoCriteria);
 			view = "informePeriodicoCursos";
+			verLibretas=true;
 			break;
 		case 4:
 			idPuerto = ServletRequestUtils.getStringParameter(request, "idPuerto", "none");
+			//Obtenemos las libretas del request
 			libreta = ServletRequestUtils.getStringParameter(request, "libreta");
+			//log.debug("El valor de 'libreta' es: "+libreta);
+			//Separamos en un arreglo
+			libretas = libreta.split(",");
+			log.debug(ToStringBuilder.reflectionToString(libretas));
 			cursoCriteria.setIdPuerto(new String[] {idPuerto});
-			cursoCriteria.setIdStatusCurso(CursoCriteria.CONCLUIDO);
+			puerto = (CatalogoPuerto)getPersistenceService().findById(CatalogoPuerto.class, idPuerto);
+			//Se lo ponemos a criteria
+			cursoCriteria.setLibretas(libretas);
+			//Lo mandamos al servicio
 			cursos = getCursoService().getCursosXStatus(cursoCriteria);
 			view = "informePeriodicoCursosAlumnos";
+			verLibretas=true;
 			break;
 		default:
 			cursos = getCursoService().getInformePeriodico(cursoCriteria.getFechaDesde(), cursoCriteria.getFechaHasta());
 			view = "informePeriodicoCursos";
 			break;
 		}
-		
-		Map model = new HashMap();
+		model.put("puerto", puerto);
+		model.put("verLibretas",verLibretas);
 		model.put("cursos", cursos);
 		model.put("criteria",cursoCriteria);
+		log.debug("Enviando el Model: "+ToStringBuilder.reflectionToString(model)+" And View: "+ view);
 		return new ModelAndView(view,model);
 	}
 	
