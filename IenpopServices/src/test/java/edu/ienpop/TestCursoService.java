dd14deb7d3ee8e295ccf655fae8d8e31ad542601
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2008-07-04 16:17:07 +0000

    Observar que existen algunos cursos que actualmente ya no se dan pero se requiere la clave para el historico
    git-svn-id: http://192.168.1.94/svn@259 d43fff5d-296d-41c8-bd8c-7388abecf751

--- IenpopServices/src/test/java/edu/ienpop/TestCursoService.java
+++ IenpopServices/src/test/java/edu/ienpop/TestCursoService.java
@@ -1,13 +1,12 @@
 package edu.ienpop;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 
+import edu.ienpop.model.Curso;
 import edu.ienpop.model.CursoCriteria;
-import edu.ienpop.model.CursoXCertificar;
 import edu.ienpop.services.BusinessException;
 import edu.ienpop.services.CursoService;
 
@@ -34,7 +33,7 @@ public class TestCursoService extends
 		cursoService.addCursoCertificado(18, "IST5¡T036EPY¿+Q?R37K");
 	}
 	
-	public void testSumaFechas(){
+	public void _testSumaFechas(){
 		Calendar cal1 = Calendar.getInstance();
 		Calendar cal2 = Calendar.getInstance();
 		cal1.set(2007, 1, 1);
@@ -45,22 +44,22 @@ public class TestCursoService extends
 	}
 	
 	@SuppressWarnings("unchecked")
-	public void _testCursoService2() throws BusinessException{
+	public void testCursoService2() throws BusinessException{
 		//cursoService.generateCertificadosXCurso(111902);
-		GregorianCalendar cal1 = new GregorianCalendar(2008,0,1);
-		GregorianCalendar cal2 = new GregorianCalendar(2008,0,30);
+		//GregorianCalendar cal1 = new GregorianCalendar(2008,0,1);
+		//GregorianCalendar cal2 = new GregorianCalendar(2008,7,30);
 		CursoCriteria cursoCriteria =  new CursoCriteria();
-		cursoCriteria.setIdPuerto(new String[]{"ACG"});
-		cursoCriteria.setIdStatusCurso(2);
-		cursoCriteria.setFechaDesde(cal1.getTime());
-		cursoCriteria.setFechaHasta(cal2.getTime());
-		List<CursoXCertificar> cursos = cursoService.getCursosXStatus(cursoCriteria);
-		for(CursoXCertificar curso:cursos){
+		cursoCriteria.setIdPuerto(new String[]{"DF"});
+		cursoCriteria.setIdStatusCurso(4);
+		//cursoCriteria.setFechaDesde(cal1.getTime());
+		//cursoCriteria.setFechaHasta(cal2.getTime());
+		List<Curso> cursos = cursoService.getCursosXStatus(cursoCriteria);
+		for(Curso curso:cursos){
 			System.out.println(curso.getId());
 		}
 	}
 	
-	public void testCursoService3() throws BusinessException{
+	public void _testCursoService3() throws BusinessException{
 		cursoService.getCursoByTokenCertificado("2BVKRIPVC3GS5P6SA21U");
 	}
 }
