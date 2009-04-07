26c5b545e6ae2efe2439cdab2931c409a6212a2f
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2009-04-07 07:41:14 +0000

    Supress Warning
    git-svn-id: http://192.168.1.94/svn@323 d43fff5d-296d-41c8-bd8c-7388abecf751

--- IenpopServices/src/main/java/edu/ienpop/services/AlumnoService.java
+++ IenpopServices/src/main/java/edu/ienpop/services/AlumnoService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import edu.ienpop.model.AlumnoCriteria;
 
 public interface AlumnoService {
+	@SuppressWarnings("unchecked")
 	List getAlumnosByCriteria(AlumnoCriteria alumnoCriteria) throws BusinessException;
 	int getCountAlumnosByCriteria(AlumnoCriteria alumnoCriteria) throws BusinessException;
 }
