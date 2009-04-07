1f518979bd4001a306805f69ac934b3cdc2260f4
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2009-04-07 07:42:19 +0000

    agregada la busqueda de alumnos a recertificar y la funcion para obtener los diplomas de los mismos...
    git-svn-id: http://192.168.1.94/svn@382 d43fff5d-296d-41c8-bd8c-7388abecf751

--- IenpopWeb/src/main/webapp/js/imprimir.js
+++ IenpopWeb/src/main/webapp/js/imprimir.js
@@ -42,7 +42,49 @@ function obtenerCursosConLlave(){
 			}
 		}
 	});
-	//alert("Si llega "+puertos);
+	//*******Obtener los cursos por recertificar********//
+	CursoXCertificarService.getCursosXCertificarReimpresion(usuarios,puertos,function(cursosAprobados){
+		dwr.util.removeAllRows("cursosRecertificadosBody", { filter:function(tr) {
+      		return (tr.id != "patternCursosRecertificados");
+		}});
+		var cursoAprobado,thisIdCursoAprobado,alumnosCursoAprobado;
+		if(cursosAprobados.length==0){
+			//Ocultamos la lista de recertificados
+			xDisplay("cursosRecertificados","none");
+		}else{
+			//Desplegamos la lista de recertificados
+			xDisplay("cursosRecertificados","block");
+			for(var i=0;i<cursosAprobados.length;i++){
+				cursoAprobado=cursosAprobados[i];
+				thisIdCursoAprobado = cursoAprobado.id;
+				alumnosCursoAprobado = cursoAprobado.alumnos;
+				dwr.util.cloneNode("patternCursosRecertificados", { idSuffix:thisIdCursoAprobado });
+				for(var j=0;j<idCursos.length;j++){
+					if(cursoAprobado.tipoCurso.idTipoCurso==idCursos[j]){
+						dwr.util.setValue("tipoCursoRecertificado"+thisIdCursoAprobado, nombreCursos[j]);
+						break;
+					}
+				}
+				//dwr.util.setValue("tipoCursoAprobado"+thisIdCursoAprobado, cursoAprobado.idTipoCurso);
+				var day=cursoAprobado.fechaInicio.getDay();
+				var month=cursoAprobado.fechaInicio.getMonth();
+				var daym=cursoAprobado.fechaInicio.getDate();
+				var year=cursoAprobado.fechaInicio.getYear();
+				year+=1900;
+				month++;//daym++;
+				var fechaCursoAprobado = daym+"/"+month+"/"+year;
+				dwr.util.setValue("fechaCursoRecertificado"+thisIdCursoAprobado, fechaCursoAprobado);
+				//alert(fechaCursoAprobado);
+				//dwr.util.setValue("noParticipantesCursoAprobado"+thisIdCursoAprobado, alumnosCursoAprobado.length);
+				$("patternCursosRecertificados" + thisIdCursoAprobado).style.display = "table-row";
+				if((i%2)==0){
+					$("patternCursosRecertificados" + thisIdCursoAprobado).style.backgroundColor = "rgb(255,255,255)";
+				}else{
+					$("patternCursosRecertificados" + thisIdCursoAprobado).style.backgroundColor = "rgb(170,213,255)";
+				}
+			}
+		}
+	});
 }
 
 function comprobarLlave(idCursoAprobado){
@@ -51,6 +93,22 @@ function comprobarLlave(idCursoAprobado){
 	//alert("Curso: "+idCursoCertificado+" Llave: "+token);
 	dwr.engine.beginBatch();
 	CursoService.addCursoCertificado(idCursoCertificado,token,function(idCurso){
+		//alert("El curso "+idCurso+" se ha certificado, imprimir los diplomas...");
+		window.open("reverso.ienpop?llave="+token+"&idCurso="+idCurso, "_blank", "width=800,height=600");
+		window.open("certificados.ienpop?idCurso="+idCurso, "_blank", "width=800,height=600");
+		obtenerCursosConLlave();
+	});
+	dwr.engine.endBatch({
+		errorHandler:function(errorString, exception) { alert(errorString+" - "+exception); }
+	});
+}
+
+function comprobarLlaveRenovada(idCursoAprobado){
+	idCursoCertificado=idCursoAprobado.substring(9);
+	token=dwr.util.getValue("llaveCertificada"+idCursoCertificado);
+	alert("Curso: "+idCursoCertificado+" Llave: "+token);
+	dwr.engine.beginBatch();
+	CursoService.updateCursoCertificado(idCursoCertificado,token,function(idCurso){
 		//alert("El curso "+idCurso+" se ha certificado, imprimir los diplomas...");
 		window.open("reverso.ienpop?llave="+token+"&idCurso="+idCurso, "_blank", "width=800,height=600");
 		window.open("certificados.ienpop?idCurso="+idCurso, "_blank", "width=800,height=600");
