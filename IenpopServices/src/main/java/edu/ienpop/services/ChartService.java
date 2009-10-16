ffcde199c436010b4ac1e657aad146fd411e8f77
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2009-10-16 20:33:48 +0000

    Primeros acercamientos a la creacion de graficas con metodos en la interfaz, implementaciones y prueba de unidad...
    git-svn-id: http://192.168.1.94/svn@413 d43fff5d-296d-41c8-bd8c-7388abecf751

--- IenpopServices/src/main/java/edu/ienpop/services/ChartService.java
+++ IenpopServices/src/main/java/edu/ienpop/services/ChartService.java
@@ -2,6 +2,11 @@ package edu.ienpop.services;
 
 import org.jfree.chart.JFreeChart;
 
+import edu.ienpop.model.AlumnoCriteria;
+import edu.ienpop.model.CursoCriteria;
+
 public interface ChartService {
-	JFreeChart generaGraficaSimple();
+	JFreeChart generaGraficaSimple() throws BusinessException;
+	JFreeChart generaGraficaCursos(CursoCriteria cursoCriteria) throws BusinessException;
+	JFreeChart generaGraficaAlumnos(AlumnoCriteria alumnoCriteria) throws BusinessException;
 }
