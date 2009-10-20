d3931d048afebbe8e38aea7d3f558b5fd842eba2
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2009-10-20 20:03:28 +0000

    Tag borrado
    git-svn-id: http://192.168.1.94/svn@419 d43fff5d-296d-41c8-bd8c-7388abecf751

--- tags/0.1-SNAPSHOT/IenpopServices/src/main/java/edu/ienpop/services/UsuarioService.java
+++
@@ -1,7 +0,0 @@
-package edu.ienpop.services;
-
-
-public interface UsuarioService {
-	boolean isValidUsuario(String idUsuario,String password) throws BusinessException;
-	String getNombreDelusuario(String usuario) throws BusinessException;
-}
