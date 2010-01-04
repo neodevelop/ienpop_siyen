a4686e1fd2f4d1c7d655c154f2c10b3d170c2e8a
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2010-01-04 20:14:59 +0000

    Cambio y reimplementacion de la implementacion de mensajeria
    git-svn-id: http://192.168.1.94/svn@447 d43fff5d-296d-41c8-bd8c-7388abecf751

--- trunk/SIYEN/ienpop-api/src/main/groovy/edu/ienpop/integration/jms/impl/IenpopProducerImpl.groovy
+++ trunk/SIYEN/ienpop-api/src/main/groovy/edu/ienpop/integration/jms/impl/IenpopProducerImpl.groovy
@@ -13,63 +13,49 @@ import org.springframework.jms.core.MessageCreator;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
+import edu.ienpop.integration.jms.IenpopProducer;
 import edu.ienpop.model.Usuario;
 import edu.ienpop.services.BusinessException;
 
-public class IenpopProducer{
-	private JmsTemplate jmsTemplate;
-	private Destination destination;
+public class IenpopProducerImpl implements IenpopProducer{
+	JmsTemplate jmsTemplate;
+	Destination destination;
 	Logger log = Logger.getLogger(this.getClass());
-	public Destination getDestination() {
-		return destination;
-	}
-	public void setDestination(Destination destination) {
-		this.destination = destination;
-	}
-	public JmsTemplate getJmsTemplate() {
-		return jmsTemplate;
-	}
-	public void setJmsTemplate(JmsTemplate jmsTemplate) {
-		this.jmsTemplate = jmsTemplate;
-	}
 	
 	public void sendMessage(String m) throws BusinessException{
 		final String message = m;
 		log.debug("Enviando el mensaje...");
-		this.jmsTemplate.send(this.getDestination(), new MessageCreator(){
-			public Message createMessage(Session session) throws JMSException {
+		this.jmsTemplate.send(this.getDestination(),[
+			createMessage:{ session -> 
 				TextMessage textMessage = session.createTextMessage();
 				textMessage.setText(message);
 				textMessage.setStringProperty("tipo", "test");
 				return textMessage;
-			}
-		});
+			}] as MessageCreator);
 	}
 	
 	public void generarLLaveQueue(long idCursoXCertificar,String email) throws BusinessException{ 
 		final String message = idCursoXCertificar+" "+email;
 		log.debug("Enviando el mensaje para certificación ...");
-		this.jmsTemplate.send(this.getDestination(), new MessageCreator(){
-			public Message createMessage(Session session) throws JMSException {
+		this.jmsTemplate.send(this.getDestination(), [
+			createMessage:{ session ->
 				TextMessage textMessage = session.createTextMessage();
 				textMessage.setText(message);
 				textMessage.setStringProperty("tipo", "generacionLlave");
 				return textMessage;
-			}
-		});
+			}] as MessageCreator);
 	}
 	
 	public void notificarAcceso(Usuario usuario) throws BusinessException{
 		XStream xstream = new XStream(new DomDriver());
 		final String xml = xstream.toXML(usuario);
 		log.debug("Enviando el mensaje para notificación ...");
-		this.jmsTemplate.send(this.getDestination(), new MessageCreator(){
-			public Message createMessage(Session session) throws JMSException {
+		this.jmsTemplate.send(this.getDestination(), [
+			createMessage:{ session ->
 				TextMessage textMessage = session.createTextMessage();
 				textMessage.setText(xml);
 				textMessage.setStringProperty("tipo", "notificacion");
 				return textMessage;
-			}
-		});
+			}] as MessageCreator);
 	}
 }
--- trunk/SIYEN/ienpop-core/src/main/java/edu/ienpop/integration/jms/IenpopProducer.java
+++ trunk/SIYEN/ienpop-core/src/main/java/edu/ienpop/integration/jms/IenpopProducer.java
@@ -3,7 +3,7 @@ package edu.ienpop.integration.jms;
 import edu.ienpop.model.Usuario;
 import edu.ienpop.services.BusinessException;
 
-public interface IenpopProducerInterface {
+public interface IenpopProducer {
 	public void sendMessage(String m) throws BusinessException;
 	public void generarLLaveQueue(long idCursoXCertificar,String email) throws BusinessException;
 	public void notificarAcceso(Usuario usuario) throws BusinessException;
