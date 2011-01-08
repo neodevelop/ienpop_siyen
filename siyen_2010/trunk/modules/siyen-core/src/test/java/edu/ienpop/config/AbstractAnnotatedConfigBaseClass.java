e2519547bd5775cf4bc1da55935b4906deef572a
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2011-01-08 06:41:17 +0000

    correccion en el archivo de configuracion, evitando confusiones...
    git-svn-id: http://192.168.1.94/svn@532 d43fff5d-296d-41c8-bd8c-7388abecf751

--- siyen_2010/trunk/modules/siyen-core/src/test/java/edu/ienpop/config/AbstractAnnotatedConfigBaseClass.java
+++ siyen_2010/trunk/modules/siyen-core/src/test/java/edu/ienpop/config/AbstractAnnotatedConfigBaseClass.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/Persistence-AppCtx.xml" })
+@ContextConfiguration(locations = { "/Annotated-AppCtx.xml" })
 public abstract class AbstractAnnotatedConfigBaseClass {
 
 }
--- siyen_2010/trunk/modules/siyen-core/src/test/java/edu/ienpop/config/AbstractJavaConfigBaseClass.java
+++ siyen_2010/trunk/modules/siyen-core/src/test/java/edu/ienpop/config/AbstractJavaConfigBaseClass.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:/PersistenceAnnotated-AppCtx.xml"})
+@ContextConfiguration(locations={"classpath:/Annotated-AppCtx.xml"})
 public abstract class AbstractJavaConfigBaseClass {
 	
 }
