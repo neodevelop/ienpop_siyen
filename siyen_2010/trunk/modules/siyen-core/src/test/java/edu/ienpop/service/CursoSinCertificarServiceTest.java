c9073bae364e0aec1b30544431b8cbfbe99fa802
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2011-01-08 06:41:30 +0000

    prueba de los metodos de CursoSinCertificarService
    git-svn-id: http://192.168.1.94/svn@552 d43fff5d-296d-41c8-bd8c-7388abecf751

--- siyen_2010/trunk/modules/siyen-core/src/test/java/edu/ienpop/service/CursoSinCertificarServiceTest.java
+++ siyen_2010/trunk/modules/siyen-core/src/test/java/edu/ienpop/service/CursoSinCertificarServiceTest.java
@@ -1,14 +1,15 @@
 package edu.ienpop.service;
 
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
+import org.junit.After;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 
-import edu.ienpop.config.AbstractJavaConfigBaseClass;
+import edu.ienpop.config.AbstractTransactionalJavaConfigBaseClass;
 import edu.ienpop.dao.InstructorDao;
 import edu.ienpop.dao.PuertoDao;
 import edu.ienpop.dao.TipoCursoDao;
@@ -20,7 +21,7 @@ import edu.ienpop.model.CursoSinCertificar;
  * 
  */
 public class CursoSinCertificarServiceTest extends
-		AbstractJavaConfigBaseClass {
+		AbstractTransactionalJavaConfigBaseClass {
 
 	/**
 	 * Inyeccion del servicio
@@ -33,9 +34,15 @@ public class CursoSinCertificarServiceTest extends
 	TipoCursoDao tipoCursoDao;
 	@Autowired
 	PuertoDao puertoDao;
-	
+
 	static CursoSinCertificar cursoSinCertificar;
-	static Set<AlumnoSinCertificar> alumnosSinCertificar;
+	static List<AlumnoSinCertificar> alumnosSinCertificar;
+
+	@After
+	public void despuesDeProbar() {
+		System.out
+				.println("**********************************************************************");
+	}
 
 	/**
 	 * Prueba la inyeccion del servicio y de sus colaboradores(daos), basados en
@@ -50,25 +57,90 @@ public class CursoSinCertificarServiceTest extends
 	 * 
 	 */
 	@Test
-	public void pruebaCrearCursoSinCertificar(){
+	public void pruebaCrearCursoSinCertificar() {
 		cursoSinCertificar = new CursoSinCertificar();
 		cursoSinCertificar.setFechaInicio(new Date());
 		cursoSinCertificar.setListoParaCertificar(false);
 		cursoSinCertificar.setPuerto(puertoDao.read("DF"));
 		cursoSinCertificar.setTipoCurso(tipoCursoDao.read("INICIA"));
 		cursoSinCertificar.setInstructor(instructorDao.read("JUANG"));
-		
+
 		AlumnoSinCertificar a1 = new AlumnoSinCertificar();
 		a1.setNombreCompleto("CRISTINA REYES ZU„IGA");
 		a1.setObservaciones("OBSERVACIONES");
 		AlumnoSinCertificar a2 = new AlumnoSinCertificar();
 		a2.setNombreCompleto("FRANCISCO RAMIREZ GARCIA");
 		a2.setObservaciones("OBSERVACIONES");
-		
-		alumnosSinCertificar = new HashSet<AlumnoSinCertificar>();
+
+		alumnosSinCertificar = new ArrayList<AlumnoSinCertificar>();
 		alumnosSinCertificar.add(a1);
 		alumnosSinCertificar.add(a2);
-		
-		cursoSinCertificarService.crearCursoSinCertificar(cursoSinCertificar, alumnosSinCertificar);
+
+		cursoSinCertificarService.crearCursoSinCertificar(cursoSinCertificar,
+				alumnosSinCertificar);
+	}
+
+	@Test
+	public void pruebaActualizaCursoSinCertificarSoloCurso() {
+		cursoSinCertificar.setFechaHoraRegistro(new Date());
+		cursoSinCertificar.setPuerto(puertoDao.read("ACG"));
+		cursoSinCertificar.setTipoCurso(tipoCursoDao.read("SEGBATUR"));
+		cursoSinCertificar.setInstructor(instructorDao.read("RAMIREZ"));
+		cursoSinCertificarService
+				.actualizaCursoSinCertificar(cursoSinCertificar);
+	}
+
+	@Test
+	public void pruebaActualizaCursoSinCertificarCursoAlumnos() {
+		cursoSinCertificar.setFechaHoraRegistro(new Date());
+		cursoSinCertificar.setPuerto(puertoDao.read("PPS"));
+		cursoSinCertificar.setTipoCurso(tipoCursoDao.read("SEGBAPES"));
+		cursoSinCertificar.setInstructor(instructorDao.read("FAUSTO"));
+
+		/**
+		 * Importante el manejo de la relaci—n entre objetos
+		 */
+		AlumnoSinCertificar a1 = new AlumnoSinCertificar();
+		a1.setNombreCompleto("RAMON ESTRADA RAMIREZ");
+		a1.setObservaciones("OBSERVACIONES");
+		a1.setCursoSinCertificar(cursoSinCertificar);
+		AlumnoSinCertificar a2 = new AlumnoSinCertificar();
+		a2.setNombreCompleto("ALICIA MACHADO VILLAREAL");
+		a2.setObservaciones("OBSERVACIONES");
+		a2.setCursoSinCertificar(cursoSinCertificar);
+
+		AlumnoSinCertificar a3 = alumnosSinCertificar.get(0);
+		a3.setCursoSinCertificar(null);
+		alumnosSinCertificar.add(a1);
+		alumnosSinCertificar.add(a2);
+		alumnosSinCertificar.remove(a3);
+		cursoSinCertificarService
+				.actualizaCursoSinCertificar(cursoSinCertificar);
+		for (AlumnoSinCertificar alumno : cursoSinCertificar
+				.getAlumnosSinCertificar()) {
+			Assert.isTrue(alumno.getIdAlumno() > 0, "El alumno viene sin ID");
+		}
+		Assert.isTrue(cursoSinCertificar.getAlumnosSinCertificar().size() == 3,
+				"No es el numero de alumnos esperado...");
+	}
+
+	@Test
+	public void pruebaObtenerCursoSinCertificarConAlumnos() {
+		Long idCurso = cursoSinCertificar.getIdCurso();
+		CursoSinCertificar curso = cursoSinCertificarService
+				.obtenerCursoSinCertificarConAlumnos(idCurso);
+		Assert.notNull(curso, "No se encontr— el curso...");
+		Assert.notEmpty(curso.getAlumnosSinCertificar());
+		Assert.isTrue(curso.getAlumnosSinCertificar().size() == 3,
+				"No es el numero de alumnos esperado...");
+	}
+
+	@Test
+	public void pruebaBorraCursoSinCertificar() {
+		cursoSinCertificarService.borraCursoSinCertificar(cursoSinCertificar
+				.getIdCurso());
+		CursoSinCertificar curso = cursoSinCertificarService
+				.obtenerCursoSinCertificarPorId(cursoSinCertificar.getIdCurso());
+		Assert.isNull(curso);
 	}
 }
