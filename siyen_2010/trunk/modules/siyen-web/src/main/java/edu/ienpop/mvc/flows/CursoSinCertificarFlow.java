3af6ccbf722886865c8f3df34b9e65433968e002
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2011-01-08 06:41:58 +0000

    Preparar relaciones para el manejo del flujo...
    git-svn-id: http://192.168.1.94/svn@591 d43fff5d-296d-41c8-bd8c-7388abecf751

--- siyen_2010/trunk/modules/siyen-web/src/main/java/edu/ienpop/mvc/flows/CursoSinCertificarFlow.java
+++ siyen_2010/trunk/modules/siyen-web/src/main/java/edu/ienpop/mvc/flows/CursoSinCertificarFlow.java
@@ -1,13 +1,12 @@
 package edu.ienpop.mvc.flows;
 
-import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.webflow.execution.RequestContext;
 
-import edu.ienpop.model.AlumnoSinCertificar;
 import edu.ienpop.model.CursoSinCertificar;
 import edu.ienpop.model.Instructor;
 import edu.ienpop.model.Puerto;
@@ -17,20 +16,27 @@ import edu.ienpop.model.TipoCurso;
 public class CursoSinCertificarFlow {
 	
 	private static final Log logger = LogFactory.getLog(CursoSinCertificarFlow.class);
-
-	public void procesaFlow(CursoSinCertificar cursoSinCertificar){
-		logger.debug(ToStringBuilder.reflectionToString(cursoSinCertificar));
-	}
 	
 	public void preparaCursoSinCertificar(CursoSinCertificar cursoSinCertificar){
 		logger.debug("Preparando el CursoSinCertificar");
 		cursoSinCertificar.setPuerto(new Puerto());
 		cursoSinCertificar.setTipoCurso(new TipoCurso());
 		cursoSinCertificar.setInstructor(new Instructor());
-		cursoSinCertificar.setAlumnosSinCertificar(new ArrayList<AlumnoSinCertificar>());
 	}
 	
-	public void showAlumnoSinCertificar(AlumnoSinCertificar alumnoSinCertificar){
-		logger.debug(ToStringBuilder.reflectionToString(alumnoSinCertificar));
+	@SuppressWarnings("unchecked")
+	public void preparaRelaciones(CursoSinCertificar cursoSinCertificar,RequestContext context){
+		//logger.debug(ToStringBuilder.reflectionToString(context));
+		logger.debug("Asignando los puerto,curso e instructor para poder persistir...");
+		List<TipoCurso> tiposCursos = (List)(context.getFlowScope().get("tiposCursos"));
+		List<Instructor> instructores = (List)(context.getFlowScope().get("instructores"));
+		List<Puerto> puertos = (List)(context.getFlowScope().get("puertos"));
+		TipoCurso tipoCurso = tiposCursos.get(tiposCursos.indexOf(cursoSinCertificar.getTipoCurso()));
+		Instructor instructor = instructores.get(instructores.indexOf(cursoSinCertificar.getInstructor()));
+		Puerto puerto = puertos.get(puertos.indexOf(cursoSinCertificar.getPuerto()));
+		cursoSinCertificar.setTipoCurso(tipoCurso);
+		cursoSinCertificar.setInstructor(instructor);
+		cursoSinCertificar.setPuerto(puerto);
 	}
+	
 }
