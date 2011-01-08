8d8a180526f79ced8abd05691198853c0c004ce6
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2011-01-08 06:41:39 +0000

    mapeada la clase Usuario para Seguridad
    git-svn-id: http://192.168.1.94/svn@567 d43fff5d-296d-41c8-bd8c-7388abecf751

--- siyen_2010/trunk/modules/siyen-api/src/main/java/edu/ienpop/model/Usuario.java
+++ siyen_2010/trunk/modules/siyen-api/src/main/java/edu/ienpop/model/Usuario.java
@@ -1,37 +1,96 @@
 package edu.ienpop.model;
 
 import java.io.Serializable;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.Cascade;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  * @author neodevelop
  *
  */
-public class Usuario implements Serializable {
+@Entity
+@Table(name="usuario")
+public class Usuario implements Serializable,UserDetails {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private String idUsuario;
+	@Id
+	private String username;
+	@Column
 	private String password;
+	@Column
+	private boolean enabled;
+	@Transient
 	private String nombre;
+	@Transient
 	private String email;
-	private Set<Instructor> instructores;
-	private Set<Puerto> puertos;
+	@Transient
+	private List<Instructor> instructores;
+	@Transient
+	private List<Puerto> puertos;
 	
-	public String getIdUsuario() {
-		return idUsuario;
+	@OneToMany(cascade = { CascadeType.MERGE }, fetch = FetchType.EAGER, mappedBy = "usuario")
+	@Cascade({org.hibernate.annotations.CascadeType.SAVE_UPDATE,
+          org.hibernate.annotations.CascadeType.DELETE_ORPHAN})
+	private List<Autoridad> autoridades;
+	
+	
+	@Override
+	public Collection<GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> auths = new ArrayList<GrantedAuthority>();
+		for(Autoridad autoridad:autoridades){
+			auths.add(new GrantedAuthorityImpl(autoridad.getAuthority()));
+		}
+		return auths;
 	}
-	public void setIdUsuario(String idUsuario) {
-		this.idUsuario = idUsuario;
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return enabled;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
 	}
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
 	}
 	public String getNombre() {
 		return nombre;
@@ -45,16 +104,22 @@ public class Usuario implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public Set<Instructor> getInstructores() {
+	public List<Instructor> getInstructores() {
 		return instructores;
 	}
-	public void setInstructores(Set<Instructor> instructores) {
+	public void setInstructores(List<Instructor> instructores) {
 		this.instructores = instructores;
 	}
-	public Set<Puerto> getPuertos() {
+	public List<Puerto> getPuertos() {
 		return puertos;
 	}
-	public void setPuertos(Set<Puerto> puertos) {
+	public void setPuertos(List<Puerto> puertos) {
 		this.puertos = puertos;
+	}
+	public List<Autoridad> getAutoridades() {
+		return autoridades;
+	}
+	public void setAutoridades(List<Autoridad> autoridades) {
+		this.autoridades = autoridades;
 	}
 }
