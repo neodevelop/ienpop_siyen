0de03c13b0614024c56679c87b723fc05b39f67c
neodevelop <neodevelop@d43fff5d-296d-41c8-bd8c-7388abecf751>
2011-01-08 06:42:12 +0000

    Implementamos CursoCertificadoService
    git-svn-id: http://192.168.1.94/svn@606 d43fff5d-296d-41c8-bd8c-7388abecf751

---
+++ siyen_2010/trunk/modules/siyen-core/src/main/java/edu/ienpop/service/impl/CursoCertificadoServiceImpl.java
@@ -0,0 +1,89 @@
+package edu.ienpop.service.impl;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+
+import edu.ienpop.dao.CursoCertificadoDao;
+import edu.ienpop.dao.CursoSinCertificarDao;
+import edu.ienpop.dao.TipoCursoDao;
+import edu.ienpop.model.AlumnoCertificado;
+import edu.ienpop.model.AlumnoSinCertificar;
+import edu.ienpop.model.CursoCertificado;
+import edu.ienpop.model.CursoCriteria;
+import edu.ienpop.model.CursoSinCertificar;
+import edu.ienpop.service.CursoCertificadoService;
+
+public class CursoCertificadoServiceImpl implements CursoCertificadoService {
+
+	private CursoSinCertificarDao cursoSinCertificarDao;
+	private CursoCertificadoDao cursoCertificadoDao;
+	private TipoCursoDao tipoCursoDao;
+
+	public CursoCertificadoServiceImpl(
+			CursoSinCertificarDao cursoSinCertificarDao,
+			CursoCertificadoDao cursoCertificadoDao, TipoCursoDao tipoCursoDao) {
+		this.cursoSinCertificarDao = cursoSinCertificarDao;
+		this.cursoCertificadoDao = cursoCertificadoDao;
+		this.tipoCursoDao = tipoCursoDao;
+	}
+
+	@Override
+	public long certificarCurso(Long idCursoSinCertificar) {
+		// Obtenemos el cursoSinCertificar
+		CursoSinCertificar cursoSinCertificar = cursoSinCertificarDao
+				.obtenerCursoSinCertificarConRelaciones(idCursoSinCertificar);
+		// Creamos la nueva instancia del curso certificado
+		CursoCertificado cursoCertificado = new CursoCertificado();
+		// No asignamos el boolean de 'certificado' por que es false por defecto
+		// Ponemos la fecha y hora del registro de este curso
+		cursoCertificado.setFechaHoraRegistro(new Date());
+		// Le decimos cuando empezo el curso
+		cursoCertificado.setFechaInicio(cursoSinCertificar.getFechaInicio());
+		// Hay que calcular la fecha de termino basada en la de inicio y en el
+		// catalogo del tipoCurso
+		// TODO Calculos
+		// Asignamos el instructor, el tipoCurso y el puerto
+		cursoCertificado.setInstructor(cursoSinCertificar.getInstructor());
+		cursoCertificado.setPuerto(cursoSinCertificar.getPuerto());
+		cursoCertificado.setTipoCurso(cursoSinCertificar.getTipoCurso());
+		// Iteramos la lista de alumnos del cursoSinCertificar y los asignamos
+		// al cursoCertificado
+		cursoCertificado.setAlumnosCertificados(new HashSet<AlumnoCertificado>());
+		for (AlumnoSinCertificar alumnoSinCertificar : cursoSinCertificar
+				.getAlumnosSinCertificar()) {
+			// Creamos el alumno certificado para agregarlo a la lista
+			AlumnoCertificado alumnoCertificado = new AlumnoCertificado();
+			// Emparejamos sus propiedades
+			alumnoCertificado.setNombreCompleto(alumnoSinCertificar.getNombreCompleto());
+			alumnoCertificado.setObservaciones(alumnoSinCertificar.getObservaciones());
+			// Establecemos la relaci—n
+			alumnoCertificado.setCursoCertificado(cursoCertificado);
+			// Lo agregamos a la lista
+			cursoCertificado.getAlumnosCertificados().add(alumnoCertificado);
+		}
+		//Finalmente persistimos el cursoCertificado
+		cursoCertificadoDao.create(cursoCertificado);
+		return cursoCertificado.getIdCurso();
+	}
+
+	@Override
+	public CursoCertificado imprimirConstanciasCurso(Long idCursoCertificado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CursoCertificado obtenerCursoCertificadoConAlumnos(Long idCurso) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CursoCertificado> obtenerCursosCertificados(
+			CursoCriteria cursoCriteria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
